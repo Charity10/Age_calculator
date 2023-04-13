8d8e721154c4cad3de0a24f2ec285bdfa7ed4998
Charity <umercy001@gmail.com>
2023-04-13 13:33:26 +0100

    feat/modified each files

--- index.js
+++ index.js
@@ -5,7 +5,13 @@ const birthYear = document.getElementById("year");
 const Display_days = document.getElementById("days");
 const Display_month = document.getElementById("months");
 const Display_year = document.getElementById("years");
-const calBtn = document.getElementById('btn')
+const yearValue = document.getElementById("yearNo");
+const monthValue = document.getElementById("monthNo");
+const dayValue = document.getElementById("dayNo");
+const calBtn = document.getElementById('btn');
+const yearNum = document.getElementById('yearNum');
+const dayNum = document.getElementById('dayNum');
+const monthNum = document.getElementById('monthNum');
 // const  = document.getElementById("form-date");
 const today = new Date();
 
@@ -16,53 +22,86 @@ let currentYear = today.getFullYear();
 
 
 const dateValidation = () => {
-        if(birthYear.value > currentYear){
+        if (birthYear.value > currentYear){
             alert('Future year not allowed')
             
-        } else {
-              
-        }
+        } else if (birthMonth.value > 12 || birthMonth.value < 1){
 
-       if(birthMonth.value > 12 || birthMonth.value < 1){
-        alert("Birth Month must be between 1 - 12")
+            alert("Birth Month must be between 1 - 12")
         
-       } else {
+       } else if (birthDay.value > 31 || birthDay.value < 1){
 
-       }
-
-      if(birthDay.value > 31 || birthDay.value < 1){
         alert("Birth Day must be between 1 - 31")
 
-      }else {
+      } else  {
 
+        if (currentDay < birthDay.value){
+
+           dayNum.innerHTML = ((currentDay - birthDay.value) + 30)
+            Display_days.innerHTML +=' Days'
+      
+          } else if(currentDay > birthDay.value){
+              
+              dayNum.innerHTML = currentDay - birthDay.value
+      
+              Display_days.innerHTML += ' Days'
+          }
+      
+          if(currentMonth < birthMonth.value){
+      
+            monthNum.innerHTML = ((currentMonth - birthMonth.value) + 12) 
+              
+            Display_month.innerHTML += 'Month' ;
+            currentYear = currentYear - 1
+      
+           } else if(currentMonth > birthMonth.value) {
+             
+              monthNum.innerHTML =  (currentMonth - birthMonth.value)
+              Display_month.innerHTML  +=  'Months';
+           }
+          
+            yearNum.innerHTML = (currentYear - birthYear.value) 
+          
+            Display_year.innerHTML += 'Years'
       }
 }
 
- calBtn.addEventListener('click', (event) => {
+calBtn.addEventListener('click', (event) =>{
+  event.preventDefault();
+  
+dateValidation()
+
+// const ageCalculator = () => {
      
-    event.preventDefault()
+    // if(currentDay < birthDay.value){
+    // dayValue.innerHTML = ((currentDay - birthDay.value) + 30)
+    //   Display_days.innerHTML =  'Days'
 
-
-    if(currentDay < birthDay.value){
-      
-      Display_days.innerHTML =  ((currentDay - birthDay.value) + 30) + 'Days'
-
-    } else if(currentDay > birthDay.value) {
+    // } else if(currentDay > birthDay.value) {
         
-         Display_days.innerHTML = currentDay - birthDay.value + ' Days'
-    }
+    //    dayValue.innerHTML = currentDay - birthDay.value
 
-    if(currentMonth < birthMonth.value){
+    //      Display_days.innerHTML = ' Days'
+    // }
+
+    // if(currentMonth < birthMonth.value){
+
+    //   monthValue.innerHTML = ((currentMonth - birthMonth.value) + 12)
         
-       Display_month.innerHTML = ((currentMonth - birthMonth.value) + 12) + 'months';
-       currentYear = currentYear - 1
+    //    Display_month.innerHTML = 'Month' ;
+    //    currentYear = currentYear - 1
 
-     } else if(currentMonth > birthMonth.value) {
- 
-          Display_month.innerHTML =  currentMonth - birthMonth.value + 'months'
-     }
+    //  } else if(currentMonth > birthMonth.value) {
+       
+    //   monthValue.innerHTML = currentMonth - birthMonth.value
+    //       Display_month.innerHTML =  'Months';
+    //  }
     
+    //   yearValue.innerHTML = (currentYear - birthYear.value) 
     
-     Display_year.innerHTML = 'Age: ' + (currentYear - birthYear.value) + " years"
-})
+    //  Display_year.innerHTML =  'Years'
+}  
+)
+
+
 
